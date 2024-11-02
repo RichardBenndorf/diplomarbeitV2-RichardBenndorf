b071640420c34c0ae64cc2c25ffc3b8d0cc816ff
RichardBenndorf <richard.benndorf@htw-dresden.de>
2024-11-02 11:58:26 +0100

    Installation commands + install GOT OCR2.0

---
+++ model/Load Model Picture Input/GOT-OCR2_0/loadGotOcr.py
@@ -0,0 +1,13 @@
+from transformers import AutoModel, AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True)
+model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
+model = model.eval().cuda()
+
+# input your test image
+image_file = '../testbilder/0.png'
+
+# plain texts OCR
+res = model.chat(tokenizer, image_file, ocr_type='ocr')
+
+print(res)
